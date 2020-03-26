34984b3c160aeb16e0d23aa4be921d8ab4590e60
Giacomo Caironi <giacomo.caironi@gmail.com>
2020-03-26 19:38:12 +0100

    Try again

--- _scripts/cibuild.sh
+++ _scripts/cibuild.sh
@@ -9,17 +9,5 @@ fi
 # enable error reporting to the console
 set -e
 
-# cleanup "_site"
-rm -rf _site
-mkdir _site
-
-# build with Jekyll into "_site"
-python3 main.py build
-
-# push
-cd _site
-git config user.email "giacomo.caironi@gmail.com"
-git config user.name "Giacomo Caironi"
-git add --all
-git commit -m "Travis #$TRAVIS_BUILD_NUMBER"
-git push --force origin master
+pip install kart
+python main.py build
