2a8359a3ff1df0bb43653a8ef8ad536e8cb05627
Giacomo Caironi <giacomo.caironi@gmail.com>
2020-03-26 19:19:20 +0100

    Try again

--- _scripts/cibuild.sh
+++ _scripts/cibuild.sh
@@ -14,7 +14,7 @@ rm -rf _site
 mkdir _site
 
 # clone remote repo to "_site"
-git clone https://${GH_TOKEN}@github.com/giacomocaironi/giacomocaironi.github.io.git --branch master _site
+git clone https://${GH_TOKEN}@github.com/giacomocaironi/giacomocaironi.github.io.git --branch main
 
 # build with Jekyll into "_site"
 python3 main.py build
