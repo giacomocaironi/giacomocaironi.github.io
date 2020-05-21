e1b2b1c8691eb80694159fe991a6b5dda0f0e94a
Giacomo Caironi <giacomo.caironi@gmail.com>
2020-05-21 23:15:52 +0200

    Custom logic to get number of posts for each tag

--- main.py
+++ main.py
@@ -3,6 +3,48 @@ from kart import miners, mappers, renderers
 
 kart = Kart()
 
+
+def blog_index(site):
+    urls = {}
+    posts = site["posts"][1:]
+    try:
+        per_page = site["config"]["pagination"]["per_page"]
+    except Exception:
+        per_page = 5
+    urls.update(
+        mappers.paginate(
+            posts, per_page, "blog_index.html", "/blog" + "/index", "blog_index"
+        )
+    )
+    return urls
+
+
+def tags(site):
+    urls = {}
+    if "tags" not in site.keys():
+        return urls
+    site["config"]["tag_post_num"] = {}
+    for tag in site["tags"]:
+        posts = site["posts"]
+        posts = [post for post in posts if tag["slug"] in post["tags"]]
+        site["config"]["tag_post_num"][tag["slug"]] = len(posts)
+        try:
+            per_page = site["config"]["pagination"]["per_page"]
+        except Exception:
+            per_page = 5
+        urls.update(
+            mappers.paginate(
+                posts,
+                per_page,
+                "tag.html",
+                "/blog" + f"/tags/{tag['slug']}",
+                f"tags.{tag['slug']}",
+                additional_data=tag,
+            )
+        )
+    return urls
+
+
 kart.miners = [
     miners.DefaultPostMiner("posts"),
     miners.DefaultCollectionMiner("tags"),
@@ -12,7 +54,10 @@ kart.miners = [
 ]
 
 kart.mappers = [
-    mappers.DefaultBlogMapper(base_url="/blog"),
+    mappers.ManualMapper(rules=[tags, blog_index]),
+    mappers.DefaultCollectionMapper(
+        base_url="/blog", collection_name="posts", template="post.html"
+    ),
     mappers.DefaultCollectionMapper(
         collection_name="projects", template="project.html"
     ),
