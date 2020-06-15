319f6ab32fba38925e111b038518816bef60890f
Giacomo Caironi <giacomo.caironi@gmail.com>
2020-06-15 18:53:39 +0200

    Update to Kart 0.6

--- main.py
+++ main.py
@@ -1,64 +1,15 @@
 from kart import Kart
-from kart import miners, mappers, renderers
-import xml.etree.ElementTree as xml
+from kart import miners, mappers, renderers, modifiers
 
 kart = Kart()
 
 
-def blog_index(site):
-    urls = {}
-    posts = site["posts"][1:]
-    try:
-        per_page = site["config"]["pagination"]["per_page"]
-    except Exception:
-        per_page = 5
-    urls.update(
-        mappers.paginate(
-            posts, per_page, "blog_index.html", "/blog" + "/index", "blog_index"
-        )
-    )
-    return urls
-
-
-def tags(site):
-    urls = {}
-    if "tags" not in site.keys():
-        return urls
-    site["config"]["tag_post_num"] = {}
+def tag_dict(site):
+    site["tag_post_count"] = {}
     for tag in site["tags"]:
         posts = site["posts"]
-        posts = [post for post in posts if tag["slug"] in post["tags"]]
-        site["config"]["tag_post_num"][tag["slug"]] = len(posts)
-        try:
-            per_page = site["config"]["pagination"]["per_page"]
-        except Exception:
-            per_page = 5
-        urls.update(
-            mappers.paginate(
-                posts,
-                per_page,
-                "tag.html",
-                "/blog" + f"/tags/{tag['slug']}",
-                f"tags.{tag['slug']}",
-                additional_data=tag,
-            )
-        )
-    return urls
-
-
-class DefaultSitemapRenderer:
-    def render(self, map, site, build_location="_site"):
-        base_url = site["config"]["base_url"]
-        root = xml.Element("urlset")
-        root.set("xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9")
-        for x in [x["url"] for x in map.values() if x["renderer"] == "main_renderer"]:
-            url = xml.SubElement(root, "url")
-            loc = xml.SubElement(url, "loc")
-            loc.text = base_url + x
-        with open(build_location + "/sitemap.xml", "w") as f:
-            f.write(
-                '<?xml version="1.0" encoding="UTF-8"?>' + xml.tostring(root).decode()
-            )
+        post_num = len([post for post in posts if tag["slug"] in post["tags"]])
+        site["tag_post_count"][tag["slug"]] = post_num
 
 
 kart.miners = [
@@ -70,10 +21,7 @@ kart.miners = [
 ]
 
 kart.mappers = [
-    mappers.ManualMapper(rules=[tags, blog_index]),
-    mappers.DefaultCollectionMapper(
-        base_url="/blog", collection_name="posts", template="post.html"
-    ),
+    mappers.DefaultBlogMapper(base_url="/blog"),
     mappers.DefaultCollectionMapper(
         collection_name="projects", template="project.html"
     ),
@@ -81,10 +29,12 @@ kart.mappers = [
     mappers.DefaultFeedMapper(),
 ]
 
+kart.modifiers = [modifiers.TagModifier(), modifiers.RuleModifier([tag_dict])]
+
 kart.renderers = [
     renderers.DefaultSiteRenderer(),
     renderers.DefaultFeedRenderer(collections=["posts", "projects"]),
-    DefaultSitemapRenderer(),
+    renderers.DefaultSitemapRenderer(),
 ]
 
 kart.config["name"] = "Giacomo Caironi"
